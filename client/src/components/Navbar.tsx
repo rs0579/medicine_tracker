bd9e5d78dd6962c87fafa59383f9318b642c372a
[Koosha Moradpour] <kushamoradpour@gmail.com>
2025-02-19 11:49:03 -0500

    Add a PatientData interface for medication.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -9,13 +9,13 @@ const Navbar = () => {
         <div
           className="container flex-row justify-space-between-lg justify-center align-center"
         >
-          <Link to="/" className="m-0"><h1 className="m-0">MedTrack</h1></Link>
-          <Button className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" radius="full">
-            <Link to="/login" className="m-0">Login</Link>
+         <Link to="/" className="m-0"><h1 className="m-0">MedTrack</h1></Link>
+          <Button className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg font-semibold" radius="full">
+            <Link to="/login" className="m-0">Login</Link> 
           </Button>
           
-          <Button className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" radius="full">
-            <Link to="/signup" className="m-0">Signup</Link>
+          <Button className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg font-semibold " radius="full">
+            <Link to="/signup" className="m-0" >Signup</Link>
           </Button>
         </div>
       </header>
