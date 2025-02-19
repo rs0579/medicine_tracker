fe5198b35d15a0a6e5ae40c1c65b673ff93af9dc
[Koosha Moradpour] <kushamoradpour@gmail.com>
2025-02-19 13:03:28 -0500

    correct landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,36 +1,13 @@
-import { useState, useEffect } from "react";
-import { retrieveFeedbacks } from "../api/feedbackAPI.tsx";
-import type { FeedbackData } from "../interfaces/FeedbackData.tsx";
-import FeedbackList from "../components/Feedbacks.tsx"
-import FeedbackForm from "../components/FeedbackForm.tsx";
-
 
 const LandingPage = () => {
 
-    // Initialize state for 'feedbacks' using useState, setting it to an empty array of type UserData.
-    const [feedbacks, setFeedbacks] = useState<FeedbackData[]>([]);
 
-    // useEffect hook runs once on component mount due to empty dependency array.
-    // It calls fetchfeedbacks to retrieve and set user data.
-    useEffect(() => {
-        fetchFeedbacks();
-    }, []);
 
-    // Async function fetchfeedbacks retrieves user data from an external source.
-    const fetchFeedbacks = async () => {
-        // Call retrievefeedbacks function which asynchronously fetches user data.
-        const data = await retrieveFeedbacks();
-        // Update 'feedbacks' state with the fetched data.
-        setFeedbacks(data);
-    }
 
-    return (
-        <>
-        <FeedbackForm />
-        <FeedbackList feedbacks={feedbacks} />
-        </>
+    return
+
         
-    );
-};
+
+}
 
 export default LandingPage;
