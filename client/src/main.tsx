bfa9ddb87c43d0b547bb835bd6ccc75196b1cd0e
[Koosha Moradpour] <kushamoradpour@gmail.com>
2025-02-19 15:25:47 -0500

    create user page and link the user page to login and signup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,9 @@ import './index.css';
 import App from './App.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
 import Home from './pages/Home.tsx';
-import Feedback from './pages/LandingPage.tsx'
 import Signup from './pages/Signup.tsx';
 import Login from './pages/Login.tsx';
+import User from './pages/User.tsx';
 
 const router = createBrowserRouter([
   {
@@ -19,18 +19,19 @@ const router = createBrowserRouter([
         index: true,
         element: <Home />
       }, 
-      {
-        path: '/feedback',
-        element: <Feedback />
-      },
+
       {
         path: '/signup',
         element: <Signup />
-      }, 
+      },
       {
-        path: '/Login',
+        path: '/login',
         element: <Login />
-      } 
+      },
+      {
+        path: '/user',
+        element: <User />
+      }
     ]
   },
 ])
